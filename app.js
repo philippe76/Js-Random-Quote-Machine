09dcba060083805d0bda4e81872ab4daef74fc71
Philippe Lanougadere <soufiane92230@gmail.com>
2021-01-25 22:33:51 +0100

    Create React basic App

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const App = () => {
+    return (
+        <div id="quote-box">
+            <p id="text">My first quote</p>
+        </div>
+    )
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('app'))
