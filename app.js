27e481118a95f4b57a7f88a01a4d40c60c9f4143
Philippe Lanougadere <soufiane92230@gmail.com>
2021-01-25 22:44:03 +0100

    Add button and useState to change displayed quote

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 
 const App = () => {
+
+    const [quote, setQuote] = React.useState('This is my first quote')
+    const [author, setAuthor] = React.useState('Unknown Author')
+
+    const fetchQuote = () => {
+        setQuote('Brand New Quote');
+        setAuthor('New Author')
+    }
+
+
     return (
         <div id="quote-box">
-            <p id="text">My first quote</p>
+            <p id="text">{quote}</p>
+            <p id="author">{author}</p>
+            <button id="new-quote" onClick={fetchQuote}>NEW QUOTE</button>
         </div>
     )
 }
