6ae2d9b62d66eb48199821d22764ac81a806a3d0
Philippe Lanougadere <soufiane92230@gmail.com>
2021-01-26 19:28:47 +0100

    Add fetching to random color API

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const App = () => {
 
     const [quote, setQuote] = React.useState('Click button below to have brand new quote')
     const [author, setAuthor] = React.useState('Unknown Author')
+    const [quoteColor, setQuoteColor] = React.useState('#6495ed')
 
     const style = {
         container: {
@@ -10,22 +11,25 @@ const App = () => {
             justifyContent: 'center',
             alignItems: 'center',
             height: '100vh',
-            fontFamily: 'Oswald'
+            fontFamily: 'Oswald',
+            backgroundColor: quoteColor
         },
         quoteBox: {
             width: '65%',
-            border: '4px solid grey',
+            backgroundColor: 'whitesmoke',
             padding: '40px 60px',
             borderRadius: '5px'
         },
         quote: {
             fontSize: '2.2rem',
             fontWeight: 'bold',
-            marginTop: 0
+            marginTop: 0,
+            color: quoteColor
         },
         author: {
             textAlign: 'right',
-            fontSize: '1.3rem'
+            fontSize: '1.3rem',
+            color: quoteColor
         },
         buttonDiv: {
             display: 'flex',
@@ -36,13 +40,13 @@ const App = () => {
         tweet: {
             fontSize: '1.5rem',
             color: 'white',
-            backgroundColor: '#6495ed',
+            backgroundColor: quoteColor,
             padding: '0.2rem 1rem',
             borderRadius: '4px',
         },
         newQuote: {
             color: 'white',
-            backgroundColor: '#6495ed',
+            backgroundColor: quoteColor,
             fontSize: '1rem',
             fontWeight: '600',
             wordSpacing: '0.2rem',
@@ -61,11 +65,20 @@ const App = () => {
             .then( response => {
                 return response.json()
             })
-        .then(data => {
-            setQuote(data.quotes[0].text);
-            setAuthor(data.quotes[0].author)
-        })   
-        .catch(err => console.log(err))    
+            .then(data => {
+                setQuote(data.quotes[0].text);
+                setAuthor(data.quotes[0].author)
+            })   
+            .catch(err => console.log(err));
+        
+        fetch('http://api.creativehandles.com/getRandomColor')
+            .then( response => {
+                return response.json()
+            })
+            .then(data => {
+                setQuoteColor(data.color);
+            })   
+            .catch(err => console.log(err));
     }
 
 
@@ -75,7 +88,7 @@ const App = () => {
                 <p id="text" style={style.quote}>" {quote} "</p>
                 <p id="author" style={style.author}>- {author} -</p>
                 <div style={style.buttonDiv}>
-                    <a href="https://twitter.com/intent/tweet"  style={style.tweet}id="tweet-quote" target="_blank" rel="noopener noreferrer"><i class="fa fa-twitter"></i></a>
+                    <a href="https://twitter.com/intent/tweet"  style={style.tweet}id="tweet-quote" target="_blank" rel="noopener noreferrer" title="Tweet this quote"><i class="fa fa-twitter"></i></a>
                     <button id="new-quote" style={style.newQuote} onClick={fetchQuote}>NEW QUOTE</button>
                 </div>
                 
