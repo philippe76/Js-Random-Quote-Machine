fbab3f94fa8bbe88d02e28e21bd2f237e56db33f
Philippe Lanougadere <soufiane92230@gmail.com>
2021-01-26 15:34:47 +0100

    Create main style part1

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
 
 const App = () => {
 
-    const [quote, setQuote] = React.useState('This is my first quote')
+    const [quote, setQuote] = React.useState('Click button below to have brand new quote')
     const [author, setAuthor] = React.useState('Unknown Author')
+
+    const style = {
+        container: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            fontFamily: 'Oswald'
+        },
+        quoteBox: {
+            width: '65%',
+            border: '4px solid grey',
+            padding: '40px 40px',
+            borderRadius: '5px'
+        },
+        quote: {
+            fontSize: '2.2rem',
+            fontWeight: 'bold',
+            marginTop: 0
+        },
+        author: {
+            textAlign: 'right',
+            fontSize: '1.2rem'
+        },
+        buttonDiv: {
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginTop: '4rem'
+        },
+        tweet: {
+            fontSize: '1.3rem',
+            color: 'white',
+            backgroundColor: '#00008b',
+            padding: '0.1rem 0.7rem',
+            borderRadius: '4px'
+        }
+        
+    }
+    
 
     const fetchQuote = () => {
         fetch('https://goquotes-api.herokuapp.com/api/v1/random?count=1')
@@ -18,10 +57,16 @@ const App = () => {
 
 
     return (
-        <div id="quote-box">
-            <p id="text">{quote}</p>
-            <p id="author">{author}</p>
-            <button id="new-quote" onClick={fetchQuote}>NEW QUOTE</button>
+        <div  style={style.container}>
+            <div id="quote-box" style={style.quoteBox}>
+                <p id="text" style={style.quote}>" {quote} "</p>
+                <p id="author" style={style.author}>- {author} -</p>
+                <div style={style.buttonDiv}>
+                    <a href="https://twitter.com/intent/tweet"  style={style.tweet}id="tweet-quote" target="_blank" rel="noopener noreferrer"><i class="fa fa-twitter"></i></a>
+                    <button id="new-quote" style={style.button} onClick={fetchQuote}>NEW QUOTE</button>
+                </div>
+                
+            </div>
         </div>
     )
 }
